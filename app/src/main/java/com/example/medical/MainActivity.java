89769eccbeb0d2e63c533b80acf50d14a9c64d62
oussamanech <oussamanach75@gmail.com>
2022-04-29 19:04:15 -0900

    Order Coffee ready - code java

--- app/src/main/java/com/example/medical/MainActivity.java
+++ app/src/main/java/com/example/medical/MainActivity.java
@@ -2,11 +2,20 @@ package com.example.medical;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView quantityTextView  ;
+    private TextView priceTextView ;
+    private EditText pricePerOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +25,48 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        quantityTextView = findViewById(R.id.quantity_text_view);
+        //priceTextView = findViewById(R.id.price_text_view);
+        pricePerOne = findViewById(R.id.price_per_one_edit_text);
 
+        int pricePerOneInt = Integer.parseInt(pricePerOne.getText().toString());
+        int quantity = Integer.parseInt(quantityTextView.getText().toString());
+        display( quantity );
+
+        displayPrice(pricePerOneInt * quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        quantityTextView = findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        priceTextView = findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+
+
+    public void increment(View view) {
+        quantityTextView = findViewById(R.id.quantity_text_view);
+        int quantity = Integer.parseInt(quantityTextView.getText().toString());
+        quantityTextView.setText("" + (quantity + 1) );
+    }
+
+    public void decrement(View view) {
+        quantityTextView = findViewById(R.id.quantity_text_view);
+        int quantity = Integer.parseInt(quantityTextView.getText().toString());
+        if(quantity>0)
+        quantityTextView.setText("" + (quantity - 1) );
+    }
 }
