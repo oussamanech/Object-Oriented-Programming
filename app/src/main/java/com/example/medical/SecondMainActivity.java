37667c8e7a522ea507ba4e557834b919170ebff0
oussamanech <oussamanach75@gmail.com>
2022-05-04 06:34:29 -0900

    Object-Oriented-Programming

--- app/src/main/java/com/example/medical/SecondMainActivity.java
+++ app/src/main/java/com/example/medical/SecondMainActivity.java
@@ -11,4 +11,7 @@ public class SecondMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.relative_layout);
     }
+    /**
+     *
+     */
 }
